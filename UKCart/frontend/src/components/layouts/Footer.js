4a2ca2474205245e5cd35d7df06314ac986ab5e3
Uttam Kumar <148858488+uttamks2003@users.noreply.github.com>
2024-10-03 03:54:47 +0530

    first commit

---
+++ UKCart/frontend/src/components/layouts/Footer.js
@@ -0,0 +1,9 @@
+export  default function Footer (){
+    return (
+        <footer className="py-1">
+            <p className="text-center text-dark mt-1">
+                UKcart - 2024-2025, All Rights Reserved
+            </p>
+        </footer>
+    )
+}
